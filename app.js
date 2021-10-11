fdaf12548075131d77b97c115fb58701613aa030
vladdyhell <vladdcantor@gmail.com>
2021-10-11 06:50:35 +0000

    change folder name from public to docs for github hosting purposes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static('public'));
+app.use(express.static('docs'));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
